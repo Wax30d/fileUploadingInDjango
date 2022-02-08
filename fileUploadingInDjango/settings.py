8193348216f3149f0a35558db14c582657c78417
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-08 11:24:30 +0500

    Created profile_maker app and connected to the project.

--- fileUploadingInDjango/settings.py
+++ fileUploadingInDjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profile_maker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
